e126563ef7d10539227615da44b0c6a673cec639
aarshpatel1 <aarsh18patel@gmail.com>
2024-09-30 12:08:17 +0530

    test add todo ui

--- script.js
+++ script.js
@@ -6,9 +6,17 @@ var todos = []
 addToDo.on("click", function () {
     todos.push(todoInput.value)
     console.log(todos)
-    todoList.innerHTML += `<li><input type="checkbox" class="form-check-input me-2"> ${todoInput.value}</li>`
+    // todoList.innerHTML += `<li><input type="checkbox" class="form-check-input me-2"> ${todoInput.value}</li>`
+    updateTodos()
 })
 
-for (let i = 0; i < todos.length; i++) {
-    console.log(todos[i])
+function updateTodos() {
+    for (let i = 0; i < todos.length; i++) {
+        todoList.innerHTML += `
+        <li class="list-group-item">
+            <input class="form-check-input" type="checkbox" value="" id="checkboxExample${i}">
+            <label class="form-check-label" for="checkboxExample${i}">${todos[i]}</label>
+        </li>`
+        console.log(todos[i])
+    }
 }
