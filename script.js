a257ecca3e766431bf7a35d34049e721bc375789
aarshpatel1 <aarsh18patel@gmail.com>
2024-09-30 12:16:19 +0530

    checkbox problem solve

--- script.js
+++ script.js
@@ -7,16 +7,14 @@ addToDo.on("click", function () {
     todos.push(todoInput.value)
     console.log(todos)
     // todoList.innerHTML += `<li><input type="checkbox" class="form-check-input me-2"> ${todoInput.value}</li>`
-    updateTodos()
+    todoList.innerHTML += `
+    <li class="list-group-item">
+        <input class="form-check-input" type="checkbox" value="" id="checkboxExample${todos.length}" />
+        <label class="form-check-label" for="checkboxExample${todos.length}">${todoInput.value}</label>
+    </li>
+    `
 })
 
-function updateTodos() {
-    for (let i = 0; i < todos.length; i++) {
-        todoList.innerHTML += `
-        <li class="list-group-item">
-            <input class="form-check-input" type="checkbox" value="" id="checkboxExample${i}">
-            <label class="form-check-label" for="checkboxExample${i}">${todos[i]}</label>
-        </li>`
-        console.log(todos[i])
-    }
+for (let i = 0; i < todos.length; i++) {
+    console.log(todos[i])
 }
